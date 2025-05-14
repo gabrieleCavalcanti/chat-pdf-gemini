acff785435f3616a6a07f2db756c9adf27a7d275
gabrieleCavalcanti <gabrielefurlancavalcanti@yahoo.com>
2025-05-14 14:45:53 -0300

    Ia de leitura de PDF

--- src/extract.py
+++ src/extract.py
@@ -1,29 +1,3 @@
-# from pypdf import PdfReader
-
-
-# def extract_text_from_pdf(pdf_file):
-#     """
-#     Função para extrair o texto de um pdf carregado o Streamlit.
-
-#     Parâmetros:
-#     pdf_file(UploadedFile)
-
-#     retorno:
-#     src: texto extraído do PDF.
-
-#     """
-#     reader = PdfReader(pdf_file)
-
-#     # reader = pypdf.Pdfreader(pdf_file) #Cria um objeto para ler o PDF
-
-#     # Percorrer todas as páginas e extrair informações
-#     text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
-
-#     return text # retorna o texto extraído
-
-
-
-
 from pypdf import PdfReader
 
 def extract_text_from_pdf(pdf_file):
